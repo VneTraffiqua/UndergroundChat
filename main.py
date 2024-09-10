817bb7d8817002b298073bea7f07ef7e97a23d18
VneTraffiqua <31010342+VneTraffiqua@users.noreply.github.com>
2024-09-10 05:56:13 -0700

    Add CLI options

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import aiofiles
 import datetime
+import argparse
 
 
 async def main():
@@ -14,4 +15,15 @@ async def main():
             await file.write(chat_with_time)
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='UndergroundChat',
+        description='Chat',
+        add_help=True
+    )
+
+    parser.add_argument('-ht', '--host', type=str, default='minechat.dvmn.org', help='Connection host used')
+    parser.add_argument('-pt', '--port', type=int, default=5000, help='Connection port used')
+    parser.add_argument('-p', '--path', type=str, default='./', help='Path to the saved chat history')
+    args = parser.parse_args()
+
     asyncio.run(main())
