61fbd185fbfe60ccd2fec7efaee79494cea7ffef
VneTraffiqua <31010342+VneTraffiqua@users.noreply.github.com>
2024-09-10 07:41:42 -0700

    Add CLI options

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ import asyncio
 import aiofiles
 import datetime
 import argparse
+from environs import Env
 
 
-async def main():
-    reader, writer = await asyncio.open_connection(host='minechat.dvmn.org', port='5000')
+async def main(host, port, file_path):
+    reader, writer = await asyncio.open_connection(host=host, port=port)
 
     while True:
         line = await reader.readline()
         chat_with_time = datetime.datetime.now().strftime('%Y-%m-%d | %H.%M.%S || ') + line.decode("utf-8")
         print(chat_with_time)
-        async with aiofiles.open(file='chat.txt', mode='a') as file:
+        async with aiofiles.open(file=file_path, mode='a') as file:
             await file.write(chat_with_time)
 
 if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+
+    HOST = env.str('HOST')
+    PORT = env.str('PORT')
+    FILE_PATH = env.str('FILE_PATH')
+
     parser = argparse.ArgumentParser(
         prog='UndergroundChat',
         description='Chat',
@@ -26,4 +34,4 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--path', type=str, default='./', help='Path to the saved chat history')
     args = parser.parse_args()
 
-    asyncio.run(main())
+    asyncio.run(main(HOST, PORT, FILE_PATH))
