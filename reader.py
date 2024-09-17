af6c4332517b58edc51496cb2088772fbac413e4
VneTraffiqua <31010342+VneTraffiqua@users.noreply.github.com>
2024-09-17 02:59:15 -0700

    Added  closing the connection

--- reader.py
+++ reader.py
@@ -2,26 +2,35 @@ import asyncio
 import aiofiles
 import datetime
 import argparse
+from contextlib import asynccontextmanager
 from environs import Env
 
 
-async def main(host, port, file_path):
+@asynccontextmanager
+async def connection_manager(host, port):
     reader, writer = await asyncio.open_connection(host=host, port=port)
+    try:
+        yield reader, writer
+    finally:
+        writer.close()
+        await writer.wait_closed()
 
-    while True:
-        line = await reader.readline()
-        chat_with_time = datetime.datetime.now().strftime('%Y-%m-%d | %H.%M.%S || ') + line.decode("utf-8")
-        print(chat_with_time)
-        async with aiofiles.open(file=file_path, mode='a') as file:
-            await file.write(chat_with_time)
+async def main(host, port, file_path):
+    async with connection_manager(host, port) as (reader, writer):
+        while True:
+            line = await reader.readline()
+            chat_with_time = datetime.datetime.now().strftime('%Y-%m-%d | %H.%M.%S || ') + line.decode("utf-8")
+            print(chat_with_time)
+            async with aiofiles.open(file=file_path, mode='a') as file:
+                await file.write(chat_with_time)
 
 if __name__ == '__main__':
     env = Env()
     env.read_env()
 
-    HOST = env.str('HOST')
-    PORT = env.str('CHAT_PORT')
-    FILE_PATH = env.str('FILE_PATH')
+    connection_host = env.str('HOST')
+    connection_port = env.str('CHAT_PORT')
+    output_file = env.str('FILE_PATH')
 
     parser = argparse.ArgumentParser(
         prog='UndergroundChat',
@@ -29,9 +38,9 @@ if __name__ == '__main__':
         add_help=True
     )
 
-    parser.add_argument('-ht', '--host', type=str, default=HOST, help='Connection host used')
-    parser.add_argument('-pt', '--port', type=int, default=PORT, help='Connection port used')
-    parser.add_argument('-p', '--path', type=str, default=FILE_PATH, help='Path to the file saved chat history')
+    parser.add_argument('-ht', '--host', type=str, default=connection_host, help='Connection host used')
+    parser.add_argument('-pt', '--port', type=int, default=connection_port, help='Connection port used')
+    parser.add_argument('-p', '--path', type=str, default=output_file, help='Path to the file saved chat history')
     args = parser.parse_args()
 
     asyncio.run(main(host=args.host, port=args.port, file_path=args.path))
--- writer.py
+++ writer.py
@@ -3,7 +3,8 @@ import argparse
 import logging
 import aiofiles
 import json
-from environs import Env, EnvError
+from environs import Env
+from contextlib import asynccontextmanager
 
 logging.basicConfig(
     format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
@@ -11,51 +12,59 @@ logging.basicConfig(
     filename='underground_chat_log.log'
 )
 
+@asynccontextmanager
+async def connection_manager(host, port):
+    reader, writer = await asyncio.open_connection(host=host, port=port)
+    try:
+        yield reader, writer
+    finally:
+        writer.close()
+        await writer.wait_closed()
+
 async def submit_message(writer, message):
     message = message.replace('\n', '').strip()
     writer.write(f'{message}\n\n'.encode())
     logging.info(msg=f'Sent message: "{message}"')
 
 async def registration(host, port, nickname):
-    reader, writer = await asyncio.open_connection(host=host, port=port)
-    await reader.readline()
-    writer.write('\n'.encode())
-    await reader.readline()
-    nickname = nickname.replace('\n', '').strip()
-    writer.write(f'{nickname}\n'.encode())
-    data = await reader.readline()
-    response = json.loads(data.decode())
-    async with aiofiles.open(file='.env', mode='a') as file:
-        await file.write(f'\nTOKEN={response["account_hash"]}')
-        logging.info(msg=f'create account hash - {response["account_hash"]} in .env file')
-    writer.close()
-    await writer.wait_closed()
+    async with connection_manager(host, port) as (reader, writer):
+        await reader.readline()
+        writer.write('\n'.encode())
+        await reader.readline()
+        nickname = nickname.replace('\n', '').strip()
+        writer.write(f'{nickname}\n'.encode())
+        data = await reader.readline()
+        response = json.loads(data.decode())
+        async with aiofiles.open(file='.env', mode='a') as file:
+            await file.write(f'\nTOKEN={response["account_hash"]}')
+            logging.info(msg=f'create account hash - {response["account_hash"]} in .env file')
+        writer.close()
+        await writer.wait_closed()
 
 async def authorise(host, port, token, message):
-    reader, writer = await asyncio.open_connection(host=host, port=port)
-    data = await reader.readline()
-    logging.info(msg=f'{data.decode()}')
-    writer.write(f'{token}\n'.encode())
-    data = await reader.readline()
-    response = json.loads(data.decode())
-    if response:
+    async with connection_manager(host, port) as (reader, writer):
+        data = await reader.readline()
         logging.info(msg=f'{data.decode()}')
-        await submit_message(writer, message)
-
-    else:
-        logging.critical(msg=f'{token} - неизвестный токен. Проверьте его или зарегистрируйте заново.')
-    await writer.drain()
-    logging.info(msg=f'Close the connection')
-    writer.close()
-    await writer.wait_closed()
+        writer.write(f'{token}\n'.encode())
+        data = await reader.readline()
+        response = json.loads(data.decode())
+        if response:
+            logging.info(msg=f'{data.decode()}')
+            await submit_message(writer, message)
+        else:
+            logging.critical(msg=f'{token} - неизвестный токен. Проверьте его или зарегистрируйте заново.')
+        await writer.drain()
+        logging.info(msg=f'Close the connection')
+        writer.close()
+        await writer.wait_closed()
 
 if __name__ == '__main__':
     env = Env()
     env.read_env()
 
-    host = env.str('HOST')
-    port = env.str('WRITER_PORT')
-    token = env.str('TOKEN')
+    connection_host = env.str('HOST')
+    connection_port = env.str('WRITER_PORT')
+    connection_token = env.str('TOKEN')
 
     parser = argparse.ArgumentParser(
         prog='UndergroundChat',
@@ -63,13 +72,13 @@ if __name__ == '__main__':
         add_help=True
     )
     parser.add_argument('your_message', type=str, help='Your message')
-    parser.add_argument('-ht', '--host', type=str, default=host, help='Connection host used')
-    parser.add_argument('-pt', '--port', type=int, default=port, help='Connection port used')
-    parser.add_argument('-t', '--token', type=str, default=token, help='Your personal hash')
+    parser.add_argument('-ht', '--host', type=str, default=connection_host, help='Connection host used')
+    parser.add_argument('-pt', '--port', type=int, default=connection_port, help='Connection port used')
+    parser.add_argument('-t', '--token', type=str, default=connection_token, help='Your personal hash')
     parser.add_argument('-n', '--nickname', type=str, default='Anonymous', help='Your nickname in chat')
 
     args = parser.parse_args()
-    if token:
+    if connection_token:
         asyncio.run(authorise(host=args.host, port=args.port, token=args.token, message=args.your_message))
     else:
         asyncio.run(registration(host=args.host, port=args.port, nickname=args.nickname))
